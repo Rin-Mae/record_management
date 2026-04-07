65cbaed7ed2ed838b58edf2cf6dce6b2a59d504d
Mae Ann Isabelle Gante <gantemaeannisabelle@gmail.com>
2026-04-07 23:05:00 +0800

    final

--- backend/database/migrations/2026_03_16_100649_create_activity_logs_table.php
+++ backend/database/migrations/2026_03_16_100649_create_activity_logs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('activity_logs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
             $table->string('model');         
             $table->unsignedBigInteger('model_id');
             $table->string('action');         
